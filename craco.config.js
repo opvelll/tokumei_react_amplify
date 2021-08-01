4b0fc8edda0abc89c4506d8da6076b25053ce76f
opvelll <opvel.hero@gmail.com>
2021-08-02 00:51:57 +0900

    fix : 開発時にminizeしない設定、ビルド時にconsole除去設定

--- craco.config.js
+++ craco.config.js
@@ -1,25 +1,24 @@
 const TerserPlugin = require("terser-webpack-plugin");
 const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   webpack: {
-    configure: {
-      optimization: {
-        minimize: isProd,
-        minimizer: isProd
-          ? [
-              new TerserPlugin({
-                terserOptions: {
-                  compress: {
-                    drop_console: true,
-                  },
-                  output: {
-                    comments: false,
-                  },
-                },
-              }),
-            ]
-          : [],
-      },
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.optimization.minimize = isProd;
+      webpackConfig.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: isProd,
+            },
+            output: {
+              comments: false,
+            },
+            mangle: false,
+          },
+        }),
+      ];
+      return webpackConfig;
     },
   },
 };
